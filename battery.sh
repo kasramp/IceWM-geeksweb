4ebea5699a013fec746c0583b772c6f3793a320e
kasramp <kasra_mp@yahoo.com>
2016-05-26 08:37:06 +0800

    small bug fixes, add suspend functionality

--- battery.sh
+++ battery.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 #upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state|to\ full|percentage"
+password=$1
 percentage=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "percentage")
 set -- $percentage
 value=$2
+value=${value%\%*}
 value=${value%.*}
 if [ $value -le 10 ]
 then
 vars=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state|time|percentage")
 set -- $vars
 #echo $vars
-zenity --warning --title="Low Power" --text=$1" "$2"\nremaining: "$6" mins\n"$8" "$9
+zenity --warning --title="Low Power" --text=$1" "$2"\nremaining: "$6" mins\n"$8" "$9 --display=:0.0
+fi
+if [ $value -le 5 ]
+then
+exec gnome-screensaver-command -l &
+echo $password | sudo -S pm-suspend
 fi
